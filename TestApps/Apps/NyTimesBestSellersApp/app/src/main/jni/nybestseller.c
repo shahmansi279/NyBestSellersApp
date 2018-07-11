a081d53800d38a495775c821413a9ba8217eed16
Mansi Shah <>
2018-07-11 12:39:08 -0700

    Beta changes to .c

--- TestApps/Apps/NyTimesBestSellersApp/app/src/main/jni/nybestseller.c
+++ TestApps/Apps/NyTimesBestSellersApp/app/src/main/jni/nybestseller.c
@@ -4,11 +4,11 @@
 JNIEXPORT jstring JNICALL
 Java_com_bestreads_NyBsClient_getBSIdentifier(JNIEnv *env, jclass type) {
 
- return (*env)->NewStringUTF(env, "36cca1784b09425492d25bbb4ef5bdf4");
+ return (*env)->NewStringUTF(env, "");
 }
 
 JNIEXPORT jstring JNICALL
 Java_com_bestreads_NyBsClient_getGIdentitifer(JNIEnv *env, jclass type) {
 
- return (*env)->  NewStringUTF(env, "AIzaSyDv7UorHAegtFuyyCzQSicwf2gMgynxyFM");
+ return (*env)->  NewStringUTF(env, "");
 }
